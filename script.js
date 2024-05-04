17522f431a1823c4f17b2c0391576733892b04fb
Coder-PinkuModi <thecoderpinku@gmail.com>
2024-05-04 14:29:17 +0530

    Script added(initial- only name in header animated) + replaces name in html in the nameheader class by ':'

---
+++ script.js
@@ -0,0 +1,13 @@
+// Here i am going to make name written in the header(animated) as typing
+const nameHeader= document.querySelector(".nameHeader");
+const text= "Pinku Modi";
+const speed=100;
+let number=0;
+function typingAnimation(){// for name in the header
+    if(number < text.length){
+        document.querySelector(".nameHeader").innerHTML += text.charAt(number);
+        number++;
+        setTimeout(typingAnimation,speed);
+    }
+}
+typingAnimation();

8c77595dc4f0deed3f5a253295ee95b78a8672a9
Coder-PinkuModi <thecoderpinku@gmail.com>
2024-05-06 21:01:25 +0530

    Added about section+services+contact with their css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Here i am going to make name written in the header(animated) as typing
 const nameHeader= document.querySelector(".nameHeader");
 const text= "Pinku Modi";
-const speed=110;
+const speed=130;
 let number=0;
 function typingAnimation(){// for name in the header
     if(number < text.length){
